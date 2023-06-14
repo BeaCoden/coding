f81d2c85630cda57a379e70abfe973c6fb88b5a9
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-06-14 13:29:49 +0200

    finished code

--- Katas/CBE/quote-app/script.js
+++ Katas/CBE/quote-app/script.js
@@ -22,10 +22,22 @@ function displayQuote(quote) {
 }
 
 // Funktion zum Abrufen eines zufälligen Zitats von der API
+function loadQuote() {
+  fetch(apiUrl) // fetch Methode zum Abrufen der API
+    .then((response) => response.json()) // .then Methode die mit promise antwortet & Daten in JSON umwandelt
+    .then((data) => {
+      // .then Methode die mit data antwortet & Daten in data speichert
+      displayQuote(data); // Aufruf der Funktion zum Anzeigen des Zitats
+    });
+}
 
 // Funktion zum Behandeln des Klickereignisses des Buttons
+function handleButtonClick() {
+  loadQuote();
+}
 
 // Event-Handler für den Klick auf den Button
+button.addEventListener("click", handleButtonClick);
 
 // Aufruf der Funktion zur Anzeige des Standardzitats
 displayDefaultQuote();
