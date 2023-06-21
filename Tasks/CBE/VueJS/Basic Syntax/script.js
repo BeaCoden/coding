39cae2be2933cd738226d3854cc07060087b22f3
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-06-22 00:08:02 +0200

    Vue.js changed code

--- Tasks/CBE/VueJS/Basic Syntax/script.js
+++ Tasks/CBE/VueJS/Basic Syntax/script.js
@@ -1,19 +1,14 @@
 // vue.createApp bedeutet, dass wir eine neue Vue-Instanz erstellen
 // Vue-Instanz
-Vue.createApp({
+const app = Vue.createApp({
   // data ist eine Funktion, die ein Objekt zurückgibt
   data() {
     return {
       message: "Hello Vue.js!", // message ist ein Property
-      count: 0, // count ist ein Property
+      userName: "Bea", // userName ist ein Property
     };
-  },
-  // methods ist ein Objekt in jeder Vue-Instanz und enthält Methoden (Funktionen)
-  methods: {
-    // Funktionen, die aufgerufen werden können
-    countUp() {
-      this.count++;
-    },
   },
   // Vue-Instanz wird an #app gemountet (angehängt)
 }).mount("#app");
+
+console.log(app.userName);
