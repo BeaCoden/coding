d96f442eb562c9e41612fe7b2ce7ce6a177c1832
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-06-22 23:06:38 +0200

    fixed bug .with html template

--- Tasks/CBE/VueJS/06-conditional-rendering/script.js
+++ Tasks/CBE/VueJS/06-conditional-rendering/script.js
@@ -1,7 +1,17 @@
 Vue.createApp({
  data() {
   return {
-   activeTab: 1,
+   activeTab: 0,
+   tabs: [
+    { name: "Tab 1", content: "This is the content of Tab 1." },
+    { name: "Tab 2", content: "This is the content of Tab 2." },
+    { name: "Tab 3", content: "This is the content of Tab 3." },
+   ],
   };
+ },
+ methods: {
+  changeTab(index) {
+   this.activeTab = index;
+  },
  },
 }).mount("#app");
