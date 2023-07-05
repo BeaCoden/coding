230c4b80cee53c583b2a8fb8553445968dc4434e
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-07-05 09:51:55 +0200

    reseted styling

--- Tasks/CBE/VueCLI/first-vue-project/src/main.js
+++ Tasks/CBE/VueCLI/first-vue-project/src/main.js
@@ -2,3 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+
+// eslint-disable-next-line no-unused-vars
+// import reset from "./assets/css/reset.css";
