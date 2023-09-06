995f007908f24270511bd58f0a1cf9e70a1a3bff
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-09-06 06:14:26 +0200

    updated

--- Tasks/CBE/VueCLI/first-vue-project/src/App.vue
+++ Tasks/CBE/VueCLI/first-vue-project/src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+	<img
+		alt="Vue logo"
+		src="./assets/logo.png"
+	/>
+	<HelloWorld msg="Welcome to Your Vue.js App" />
+	<MyFirstComponent />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+	import HelloWorld from "./components/HelloWorld.vue";
+	import MyFirstComponent from "./components/MyFirstComponent.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+	export default {
+		name: "App",
+		components: {
+			HelloWorld,
+			MyFirstComponent,
+		},
+	};
 </script>
---
+++ Tasks/CBE/VueCLI/first-vue-project/src/components/MyFirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>My First Component</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "MyFirstComponent",
+		data() {
+			return {
+				msg: "Welcome to Your Vue.js App",
+			};
+		},
+	};
+</script>
